010adc471d534d8aabf30a07a5c3a091ba19e1cc
Ryan Tunis <rtunis@niagaracollege.ca>
2017-01-26 20:56:51 -0500

    Finished tutorial.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Book 1', pages: 100 },
+        { title: 'Another Book', pages: 324 },
+        { title: 'More Books', pages: 38 },
+        { title: 'Last Book', pages: 25 }
+    ];
+}
